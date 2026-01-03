200152e08bfd5fb95899d898759eaa2ce125c367
Abdullah <abdullahrehman1019@gmail.com>
2026-01-02 19:59:53 -0600

    initial commit

---
+++ lib/services/task.dart
@@ -0,0 +1,192 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:sarmad_apis/models/task.dart';
+import 'package:sarmad_apis/models/taskListing.dart';
+
+class TaskServices{
+  String baseURL = "https://todo-nu-plum-19.vercel.app/";
+
+  ///Create Task
+  Future<TaskModel> createTask({
+    required String description,
+    required String token,}
+      )async{
+    try{
+      http.Response response =
+      await http.post(
+        Uri.parse("$baseURL/todos/add"),
+        headers: {
+          "Content-Type":"application/json",
+          'Authorization': token
+        },
+        body: jsonEncode({
+          "description": description,
+        }),);
+      if(response.statusCode == 201 || response.statusCode == 200){
+        return TaskModel.fromJson(jsonDecode(response.body));}
+      else{
+        throw Exception("Something went wrong");
+      }
+    }catch(e){
+      throw e.toString();
+    }
+  }
+  /// Get All Task
+  Future<TaskListingModel> getAllTask(String token)async{
+    try{
+      http.Response response =
+      await http.get(
+        Uri.parse("$baseURL/todos/get"),
+        headers: {
+          'Authorization': token
+        },
+        );
+      if(response.statusCode == 201 || response.statusCode == 200){
+        return TaskListingModel.fromJson(jsonDecode(response.body));}
+      else{
+        throw Exception("Something went wrong");
+      }
+    }catch(e){
+      throw e.toString();
+    }
+  }
+  /// Get Completed Task
+  Future<TaskListingModel> getCompletedTask(String token)async{
+    try{
+      http.Response response =
+      await http.get(
+        Uri.parse("$baseURL/todos/completed"),
+        headers: {
+          'Authorization': token
+        },
+      );
+      if(response.statusCode == 201 || response.statusCode == 200){
+        return TaskListingModel.fromJson(jsonDecode(response.body));}
+      else{
+        throw Exception("Something went wrong");
+      }
+    }catch(e){
+      throw e.toString();
+    }
+  }
+  /// Get InCompleted Task
+  Future<TaskListingModel> getInCompletedTask(String token)async{
+    try{
+      http.Response response =
+      await http.get(
+        Uri.parse("$baseURL/todos/incomplete"),
+        headers: {
+          'Authorization': token
+        },
+      );
+      if(response.statusCode == 201 || response.statusCode == 200){
+        return TaskListingModel.fromJson(jsonDecode(response.body));}
+      else{
+        throw Exception("Something went wrong");
+      }
+    }catch(e){
+      throw e.toString();
+    }
+  }
+  ///Update Task
+  Future<bool> updateTask({
+    required String token,
+    required String taskID,
+    required String description
+})async{
+    try{
+      http.Response response =
+      await http.patch(
+        Uri.parse("$baseURL/todos/update/$taskID"),
+        headers: {
+          'Authorization': token
+        },
+        body: jsonEncode({
+          "description": description,
+        }),
+      );
+      if(response.statusCode == 201 || response.statusCode == 200){
+        return true;
+      }
+      else{
+        throw Exception("Something went wrong");
+      }
+    }catch(e){
+      throw e.toString();
+    }
+  }
+  ///Delete Task
+  Future<bool> deleteTask({
+    required String token,
+    required String taskID,
+  })async{
+    try{
+      http.Response response =
+      await http.delete(
+        Uri.parse("$baseURL/todos/delete/$taskID"),
+        headers: {
+          'Authorization': token,
+          'Content-Type': 'application/json'
+        },
+      );
+      if(response.statusCode == 201 || response.statusCode == 200){
+        return true;
+      }
+      else{
+        throw Exception("Something went wrong");
+      }
+    }catch(e){
+      throw e.toString();
+    }
+  }
+
+  ///Search Task
+  Future<TaskListingModel> searchTask({
+    required String token,
+    required String keyword,
+  })async{
+    try{
+      http.Response response =
+      await http.get(
+        Uri.parse("$baseURL/todos/search?keywords=$keyword"),
+        headers: {
+          'Authorization': token,
+        },
+      );
+      if(response.statusCode == 201 || response.statusCode == 200){
+        return TaskListingModel.fromJson(jsonDecode(response.body));
+      }
+      else{
+        throw Exception("Something went wrong");
+      }
+    }catch(e){
+      throw e.toString();
+    }
+  }
+  ///Filter Task
+  Future<TaskListingModel> filterTask({
+    required String token,
+    required String startDate,
+    required String endDate,
+  })async{
+    try{
+      http.Response response =
+      await http.get(
+        Uri.parse("$baseURL/todos/filter?startDate=$startDate&endDate=$endDate"),
+        headers: {
+          'Authorization': token,
+        },
+      );
+      if(response.statusCode == 201 || response.statusCode == 200){
+        return TaskListingModel.fromJson(jsonDecode(response.body));
+      }
+      else{
+        throw Exception("Something went wrong");
+      }
+    }catch(e){
+      throw e.toString();
+    }
+  }
+}
+
+
