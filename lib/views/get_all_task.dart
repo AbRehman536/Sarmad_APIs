56caefb4f877744d3106b60b4c992edca0dc476c
Abdullah <abdullahrehman1019@gmail.com>
2026-01-07 13:01:58 -0600

    Update and Delete Done

--- lib/views/get_all_task.dart
+++ lib/views/get_all_task.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sarmad_apis/models/task.dart';
-import 'package:sarmad_apis/models/taskListing.dart';
+import 'package:sarmad_apis/models/taskListing.dart' hide Task;
 import 'package:sarmad_apis/services/task.dart';
 import 'package:sarmad_apis/views/get_completed_task.dart';
 import 'package:sarmad_apis/views/get_incompleted_task.dart';
+import 'package:sarmad_apis/views/update_task.dart';
 
 import '../provider/user_token_provider.dart';
 
@@ -37,6 +38,34 @@ class GetAllTask extends StatelessWidget {
               return ListTile(
                 leading: Icon(Icons.task),
                 title: Text(taskListingModel.tasks![index].description.toString()),
+                trailing: Row(children: [
+                  IconButton(onPressed: ()async{
+                    try{
+                      await TaskServices().deleteTask(
+                          token: userProvider.getToken().toString(),
+                          taskID: taskListingModel.tasks![index].id.toString())
+                          .then((value){
+                            showDialog(context: context, builder: (BuildContext context) {
+                              return AlertDialog(
+                                content: Text("Deleted Successfully"),
+                                actions: [
+                                  TextButton(onPressed: (){
+                                    Navigator.pop(context);
+                                  }, child: Text("Okay"))
+                                ],
+                              );
+                            }, );
+                      });
+                    }catch(e){
+                      ScaffoldMessenger.of(context)
+                          .showSnackBar(SnackBar(content: Text(e.toString())));
+                    }
+
+                  }, icon: Icon(Icons.delete)),
+                  IconButton(onPressed: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=> UpdateTask(model: Task(),)));
+                  }, icon: Icon(Icons.edit))
+                ],),
               );
             },);
        },
